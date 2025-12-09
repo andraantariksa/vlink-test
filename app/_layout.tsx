a7f677ce2d2522cd0a976400c41e52a41150519f
Andra Antariksa <andra@stamps.id>
2025-12-09 20:47:01 +0700

    feat: signin screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,13 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="screens/signin/index"
+        options={{ title: "Sign In" }}
+      />
+      <Stack.Screen name="screens/home/index" options={{ title: "Home" }} />
+    </Stack>
+  );
 }
